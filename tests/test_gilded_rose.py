26f292f2fffe39a245f6792044b43003c90b2e5b
Alfonso Ramirez <alfonso.ramirez@ebury.com>
2025-11-21 18:07:13 +0100

    test: add test to check normal item quality procedure

--- tests/test_gilded_rose.py
+++ tests/test_gilded_rose.py
@@ -1,8 +1,12 @@
+import pytest
+
+from available_items import NORMAL_ITEM, BACKSTAGE_PASS, AGED_BRIE, SULFURAS
 from gilded_rose import GildedRose, Item
 
 
-def test_foo():
-    item = Item("foo", 0, 0)
+def test_normal_item_quality_degrades_one_point_before_sell_date():
+    item = Item(name=NORMAL_ITEM, sell_in=10, quality=10)
     gilded_rose = GildedRose([item])
     gilded_rose.update_quality()
     assert item.name == "foo"
+    assert item.quality == 9
