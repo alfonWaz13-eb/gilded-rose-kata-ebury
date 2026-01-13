42ad7c10566d800ac1fa4654968b1790367aea8d
Carlos <carlos.dominguez@ebury.com>
2026-01-13 12:17:34 +0100

    add: constants file

---
+++ constants.py
@@ -0,0 +1,9 @@
+MAX_QUALITY = 50
+MIN_QUALITY = 0
+
+
+# Item name constants
+AGED_BRIE = "Aged Brie"
+BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
+SULFURAS = "Sulfuras, Hand of Ragnaros"
+CONJURED_PREFIX = "conjured"
