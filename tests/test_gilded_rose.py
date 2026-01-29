6edb7bbd566101165614a6eea46fc465ea055fbc
Juan Javier García <juan.garcia.external@ebury.com>
2026-01-29 16:02:50 +0100

    add more test

--- tests/test_gilded_rose.py
+++ tests/test_gilded_rose.py
@@ -7,3 +7,39 @@ def test_general_item_reduces_quality_every_day():
     gilded_rose.update_quality()
     assert item.sell_in == 9
     assert item.quality == 9
+
+def test_general_item_never_goes_below_zero():
+    item = Item("foo", 0, 0)
+    gilded_rose = GildedRose([item])
+    gilded_rose.update_quality()
+    assert item.sell_in == -1
+    assert item.quality == 0
+
+def test_sulfuras_quality_always_80():
+    item = Item("Sulfuras, Hand of Ragnaros", 0, 80)
+    gilded_rose = GildedRose([item])
+    gilded_rose.update_quality()
+    assert item.sell_in == 0
+    assert item.quality == 80
+
+def test_backstage_15_sell_in_increase_quality_1():
+    item = Item("Backstage passes to a TAFKAL80ETC concert", 15, 40)
+    gilded_rose = GildedRose([item])
+    gilded_rose.update_quality()
+    assert item.sell_in == 14
+    assert item.quality == 41
+
+def test_backstage_10_sell_in_increase_quality_2():
+    item = Item("Backstage passes to a TAFKAL80ETC concert", 10, 40)
+    gilded_rose = GildedRose([item])
+    gilded_rose.update_quality()
+    assert item.sell_in == 9
+    assert item.quality == 42
+
+def test_backstage_5_sell_in_increase_quality_3():
+    item = Item("Backstage passes to a TAFKAL80ETC concert", 5, 40)
+    gilded_rose = GildedRose([item])
+    gilded_rose.update_quality()
+    assert item.sell_in == 4
+    assert item.quality == 43
+
