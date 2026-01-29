c67f647c9e2ac5693031540bba91d03ed47d337e
manuelridao-ebury <manuel.ridao@ebury.com>
2026-01-29 16:46:00 +0100

    added item classes

--- gilded_rose.py
+++ gilded_rose.py
@@ -7,33 +7,19 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            if item.name == "Aged Brie":
+                updater = AgedBrieUpdater()
+                updater.update_quality(item)
+                return
+
+            if item.name == "Sulfuras, Hand of Ragnaros":
+                updater = SulfurasUpdater()
+                updater.update_quality(item)
+                return
+
+            updater = Updater()
+            updater.update_quality(item)
+            
 
 
 class Item:
@@ -44,3 +30,19 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+
+class Updater:
+    def update_quality(self, item):
+        item.sell_in = item.sell_in - 1
+        item.quality = item.quality - 1
+
+class AgedBrieUpdater(Updater):
+    def update_quality(self, item):
+        if item.quality < 50:
+            item.quality = item.quality + 1
+        item.sell_in = item.sell_in - 1
+
+class SulfurasUpdater(Updater):
+    def update_quality(self, item):
+        return
