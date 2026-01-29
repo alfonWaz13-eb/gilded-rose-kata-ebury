feaf08b019c1c10ae1589b3909c28d6916ad104c
enrique <enrique.ladron.external@ebury.com>
2026-01-29 15:46:32 +0100

    100% coverage

--- tests/test_gilded_rose.py
+++ tests/test_gilded_rose.py
@@ -2,7 +2,12 @@ from gilded_rose import GildedRose, Item
 
 
 def test_foo():
-    item = Item("foo", 0, 0)
-    gilded_rose = GildedRose([item])
+    items = list()
+    items.append(Item("foo", 0, 10))
+    items.append(Item("Aged Brie", 0, 0))
+    items.append(Item("Backstage passes to a TAFKAL80ETC concert", 0, 0))
+    items.append(Item("Sulfuras, Hand of Ragnaros", 0, 80))
+    gilded_rose = GildedRose(items)
     gilded_rose.update_quality()
-    assert item.name == "foo"
+    for item in items:
+        assert item.__repr__() in ["foo, -1, 8", "Aged Brie, -1, 2", "Backstage passes to a TAFKAL80ETC concert, -1, 0", "Sulfuras, Hand of Ragnaros, 0, 80"]
