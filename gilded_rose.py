055df7a8f5db60a9d238663129831490e77eb971
Javier Garcia <javier.garcia@ebury.com>
2026-01-29 17:00:52 +0100

    Extracting methods

--- gilded_rose.py
+++ gilded_rose.py
@@ -25,34 +25,47 @@ class GildedRose(object):
     def update_quality(self):
         for item in self.items:
             if item.name == SULFURAS:
-                item.quality = 80
+                self.update_sulfuras(item)
             elif item.name == AGED_BRIE:
-                if item.sell_in < 0:
-                    item.quality += AGED_BRIE_QUALITY_INCREASE_NEGATIVE_SELLIN
-                elif item.quality < TOP_QUALITY:
-                    item.quality += DEFAULT_QUALITY_UNIT
-                item.sell_in -= DEFAULT_SELL_IN_UNIT
+                self.update_aged_brie(item)
             elif item.name == CONCERT_TICKETS:
-                if item.sell_in < 0:
-                    item.quality = MIN_QUALITY
-                elif item.sell_in <= VERY_CLOSE_DATE_DAYS_LIMIT:
-                    item.quality += VERY_CLOSE_DATE_QUALITY_INCREASE
-                elif item.sell_in <= CLOSE_DATE_DAYS_LIMIT:
-                    item.quality += CLOSE_DATE_QUALITY_INCREASE
-                else:
-                    item.quality += DEFAULT_QUALITY_UNIT
-                item.sell_in -= DEFAULT_SELL_IN_UNIT
+                self.update_concert_tickets(item)
             else:
-                if item.sell_in < 0:
-                    item.quality -= DOUBLE_QUALITY_UNIT
-                    item.sell_in -= DEFAULT_SELL_IN_UNIT
-                else:
-                    item.sell_in -= DEFAULT_SELL_IN_UNIT
-                    item.quality -= DEFAULT_QUALITY_UNIT
-                if item.quality < MIN_QUALITY:
-                    item.quality = MIN_QUALITY
-                elif item.quality > TOP_QUALITY:
-                    item.quality = TOP_QUALITY
+                self.update_normal_item(item)
+
+    def update_normal_item(self, item):
+        if item.sell_in < 0:
+            item.quality -= DOUBLE_QUALITY_UNIT
+            item.sell_in -= DEFAULT_SELL_IN_UNIT
+        else:
+            item.sell_in -= DEFAULT_SELL_IN_UNIT
+            item.quality -= DEFAULT_QUALITY_UNIT
+        if item.quality < MIN_QUALITY:
+            item.quality = MIN_QUALITY
+        elif item.quality > TOP_QUALITY:
+            item.quality = TOP_QUALITY
+
+    def update_sulfuras(self, item):
+        item.quality = 80
+
+    def update_aged_brie(self, item):
+        if item.sell_in < 0:
+            item.quality += AGED_BRIE_QUALITY_INCREASE_NEGATIVE_SELLIN
+        elif item.quality < TOP_QUALITY:
+            item.quality += DEFAULT_QUALITY_UNIT
+        item.sell_in -= DEFAULT_SELL_IN_UNIT
+
+    def update_concert_tickets(self, item):
+        if item.sell_in < 0:
+            item.quality = MIN_QUALITY
+        elif item.sell_in <= VERY_CLOSE_DATE_DAYS_LIMIT:
+            item.quality += VERY_CLOSE_DATE_QUALITY_INCREASE
+        elif item.sell_in <= CLOSE_DATE_DAYS_LIMIT:
+            item.quality += CLOSE_DATE_QUALITY_INCREASE
+        else:
+            item.quality += DEFAULT_QUALITY_UNIT
+        item.sell_in -= DEFAULT_SELL_IN_UNIT
+
 
 class Item:
     def __init__(self, name, sell_in, quality):
