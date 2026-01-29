65ccc811400a144c2354ccfd558e9c9a5c7bddbc
mara <mara.gambero@ebury.com>
2026-01-29 16:46:01 +0100

    Create items classes

--- gilded_rose.py
+++ gilded_rose.py
@@ -5,7 +5,7 @@ class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
-    def update_quality(self):
+    def _update_quality(self):
         for item in self.items:
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality > 0:
@@ -35,12 +35,50 @@ class GildedRose(object):
                     if item.quality < 50:
                         item.quality = item.quality + 1
 
+    def update_quality(self):
+        for item in self.items:
+            item.update_item()
+
 
 class Item:
-    def __init__(self, name, sell_in, quality):
+    def __init__(self, name, sell_in, quality, sell_in_change_rate=-1):
         self.name = name
         self.sell_in = sell_in
         self.quality = quality
+        self._change_rate = lambda x: -1 if self.sell_in >= 0 else -2
+        self._sell_in_change_rate = sell_in_change_rate
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+    def _update_quality(self):
+        self.quality += self._change_rate()
+
+    def _check_quality_limits(self):
+        self.quality = max(min(self.quality, 50), 0)
+
+    def _update_sell_in(self):
+        self.sell_in += self._sell_in_change_rate
+
+    def update_item(self):
+        self._update_sell_in()
+        self._update_quality()
+        self._check_quality_limits()
+
+
+class AgedBrie(Item):
+    def __init__(self, **kwargs):
+        super().__init__(name="Aged Brie", **kwargs)
+        self._change_rate = lambda x: 1 if self.sell_in >= 0 else 2
+
+
+class Sulfuras(Item):
+    def __init__(self, **kwargs):
+        super().__init__(name = "Sulfuras, Hand of Ragnaros", quality=80, sell_in_change_rate=0, **kwargs)
+        self._change_rate = lambda x: 0
+
+
+class BackstagePasses(Item):
+    def __init__(self, **kwargs):
+        super().__init__(name="Backstage Passes", **kwargs)
+        self._change_rate = lambda x: 1 if self.sell_in >= 11 else 2 if self.sell_in >= 6 else 3
