72dc6f0804cd9f19a76131663afbecfa47b1818c
Juan Javier García <juan.garcia.external@ebury.com>
2026-01-29 16:20:22 +0100

    refactor, extract names to constants

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+SULFURAS = "Sulfuras, Hand of Ragnaros"
+CONCERT_TICKETS = "Backstage passes to a TAFKAL80ETC concert"
+AGED_BRIE = "Aged Brie"
+
 
 class GildedRose(object):
 
@@ -7,27 +11,27 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+            if item.name != AGED_BRIE and item.name != CONCERT_TICKETS:
                 if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
+                    if item.name != SULFURAS:
                         item.quality = item.quality - 1
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    if item.name == CONCERT_TICKETS:
                         if item.sell_in < 11:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
                         if item.sell_in < 6:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
+            if item.name != SULFURAS:
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
+                if item.name != AGED_BRIE:
+                    if item.name != CONCERT_TICKETS:
                         if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
+                            if item.name != SULFURAS:
                                 item.quality = item.quality - 1
                     else:
                         item.quality = item.quality - item.quality
