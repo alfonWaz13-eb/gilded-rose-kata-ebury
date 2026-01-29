66e2cd913b4fb88db9fcdf7118a20c86b21c492a
manuelridao-ebury <manuel.ridao@ebury.com>
2026-01-29 15:43:10 +0100

    added 3 initial tests

--- tests/test_gilded_rose.py
+++ tests/test_gilded_rose.py
@@ -2,7 +2,25 @@ from gilded_rose import GildedRose, Item
 
 
 def test_foo():
-    item = Item("foo", 0, 0)
+    item = Item("foo", 1, 1)
     gilded_rose = GildedRose([item])
     gilded_rose.update_quality()
     assert item.name == "foo"
+    assert item.sell_in == 0
+    assert item.quality == 0
+
+def test_aged_brie():
+    item = Item("Aged Brie", 10, 0)
+    gilded_rose = GildedRose([item])
+    gilded_rose.update_quality()
+    assert item.name == "Aged Brie"
+    assert item.sell_in == 9
+    assert item.quality == 1
+
+def test_sulfuras():
+    item = Item("Sulfuras, Hand of Ragnaros", 1, 80)
+    gilded_rose = GildedRose([item])
+    gilded_rose.update_quality()
+    assert item.name == "Sulfuras, Hand of Ragnaros"
+    assert item.sell_in == 1
+    assert item.quality == 80
