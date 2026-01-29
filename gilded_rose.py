9249be3db025a3da9eb59e5a86248a4862898c7f
enrique <enrique.ladron.external@ebury.com>
2026-01-29 17:02:14 +0100

    splitting into child classes

--- gilded_rose.py
+++ gilded_rose.py
@@ -7,33 +7,7 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            item.update_item()
 
 
 class Item:
@@ -44,3 +18,87 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+    def update_item(self):
+        """
+        - Every item loses **1 Quality** and **1 SellIn** each day.
+        - If the sell-by date is **past** (SellIn < 0), **Quality** drops **twice as fast** (–2/day).
+        - **Quality never goes below 0**, and never above **50** (some exceptions).
+        """
+        if self.sell_in < 0:
+            self.quality -= 2
+        else:
+            self.quality -= 1
+
+        if self.quality < 0:
+            self.quality = 0
+
+        self.sell_in -= 1
+
+class AgedBrie(Item):
+    name = "Aged Brie"
+    def update_item(self):
+        """
+        - Gets better with age: **Quality increases** each day (up to 50).
+        - if sellin <0 in aged brie, quality increases in two.
+        """
+        if self.sell_in < 0:
+            self.quality += 2
+        else:
+            self.quality += 1
+
+        if self.quality > 50:
+            self.quality = 50
+
+        self.sell_in -= 1
+
+class Backstage(Item):
+    name = "Backstage passes to a TAFKAL80ETC concert"
+    def update_item(self):
+        """
+        - Improve as the concert approaches:
+        - +1 Quality per day
+        - If 10 days left: **+2 Quality**
+        - If 5 days left: **+3 Quality**
+        - **After the concert (SellIn < 0): Quality drops to 0**
+        """
+        if self.sell_in > 5 and self.sell_in <= 10:
+            self.quality += 2
+        elif self.sell_in >= 0 and self.sell_in <= 5:
+            self.quality += 3
+        elif self.sell_in < 0:
+            self.quality = 0
+        else:
+            self.quality += 1
+
+        if self.quality > 50:
+            self.quality = 50
+
+        self.sell_in -= 1
+
+
+class Conjured(Item):
+    name = "Conjured Item"
+    def update_item(self):
+        """
+        - Spoil faster: **Quality drops twice as fast as normal items**.
+        """
+        if self.sell_in < 0:
+            self.quality -= 4
+        else:
+            self.quality -= 2
+
+        if self.quality < 0:
+            self.quality = 0
+
+        self.sell_in -= 1
+
+class Sulfuras(Item):
+    name = "Sulfuras, Hand of Ragnaros"
+    def update_item(self):
+        """
+        - Legendary, never sold!
+        - **Quality always 80** and never changes.
+        """
+        self.sell_in -= 1
+        self.quality = 80
--- tests/test_gilded_rose.py
+++ tests/test_gilded_rose.py
@@ -1,13 +1,20 @@
-from gilded_rose import GildedRose, Item
+from gilded_rose import GildedRose, Item, AgedBrie, Backstage, Sulfuras, Conjured
 
 
 def test_foo():
     items = list()
     items.append(Item("foo", 0, 10))
-    items.append(Item("Aged Brie", 0, 0))
-    items.append(Item("Backstage passes to a TAFKAL80ETC concert", 0, 0))
-    items.append(Item("Sulfuras, Hand of Ragnaros", 0, 80))
+    items.append(AgedBrie("Aged Brie", 0, 0))
+    items.append(Backstage("Backstage passes to a TAFKAL80ETC concert", 0, 0))
+    items.append(Conjured("Conjured Item", 0, 4))
+    items.append(Sulfuras("Sulfuras, Hand of Ragnaros", 0, 80))
     gilded_rose = GildedRose(items)
     gilded_rose.update_quality()
     for item in items:
-        assert item.__repr__() in ["foo, -1, 8", "Aged Brie, -1, 2", "Backstage passes to a TAFKAL80ETC concert, -1, 0", "Sulfuras, Hand of Ragnaros, 0, 80"]
+        assert item.__repr__() in [
+            "foo, -1, 9",
+            "Aged Brie, -1, 1",
+            "Backstage passes to a TAFKAL80ETC concert, -1, 3",
+            "Conjured Item, -1, 2",
+            "Sulfuras, Hand of Ragnaros, -1, 80"
+        ]
