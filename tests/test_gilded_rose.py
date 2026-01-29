f860299f68c36d5b9f3d21e303b265cdf1f5af02
manuelridao-ebury <manuel.ridao@ebury.com>
2026-01-29 16:00:21 +0100

    added backstage scaffold tests

--- tests/test_gilded_rose.py
+++ tests/test_gilded_rose.py
@@ -34,7 +34,7 @@ def test_quality_max():
     assert item.quality == 50
 
 # special items tests
-def test_aged_brie():
+def test_aged_brie_quality_increase():
     item = Item("Aged Brie", 10, 0)
     gilded_rose = GildedRose([item])
     gilded_rose.update_quality()
@@ -42,7 +42,39 @@ def test_aged_brie():
     assert item.sell_in == 9
     assert item.quality == 1
 
-def test_sulfuras():
+def test_sulfuras_never_changes():
+    item = Item("Sulfuras, Hand of Ragnaros", 1, 80)
+    gilded_rose = GildedRose([item])
+    gilded_rose.update_quality()
+    assert item.name == "Sulfuras, Hand of Ragnaros"
+    assert item.sell_in == 1
+    assert item.quality == 80
+
+def test_backstage_quality_increase():
+    item = Item("Backstage passes to a TAFKAL80ETC concert", 1, 80)
+    gilded_rose = GildedRose([item])
+    gilded_rose.update_quality()
+    assert item.name == "Backstage passes to a TAFKAL80ETC concert"
+    assert item.sell_in == 0
+    assert item.quality == 80
+
+def test_backstage_quality_increase_10_days():
+    item = Item("Sulfuras, Hand of Ragnaros", 1, 80)
+    gilded_rose = GildedRose([item])
+    gilded_rose.update_quality()
+    assert item.name == "Sulfuras, Hand of Ragnaros"
+    assert item.sell_in == 1
+    assert item.quality == 80
+
+def test_backstage_quality_increase_5_days():
+    item = Item("Sulfuras, Hand of Ragnaros", 1, 80)
+    gilded_rose = GildedRose([item])
+    gilded_rose.update_quality()
+    assert item.name == "Sulfuras, Hand of Ragnaros"
+    assert item.sell_in == 1
+    assert item.quality == 80
+
+def test_backstage_quality_zero():
     item = Item("Sulfuras, Hand of Ragnaros", 1, 80)
     gilded_rose = GildedRose([item])
     gilded_rose.update_quality()
